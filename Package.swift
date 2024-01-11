f2e0caf5261286917329044c45443feec86804bc
Niels G <Nielssg@users.noreply.github.com>
2024-01-11 13:29:30 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         .target(
             name: "MaterialComponents",
             dependencies: [],
-            path:"Library"
+            path:"Components"
         )
     ]
 )
