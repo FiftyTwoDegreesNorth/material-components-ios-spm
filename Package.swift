7732c23c596a51f4ab57bbb26602fc66b5a091f4
Niels G <Nielssg@users.noreply.github.com>
2024-01-11 13:27:47 +0100

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MaterialComponents",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "MaterialComponents",
+            targets: ["MaterialComponents"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "MaterialComponents",
+            dependencies: [],
+            path:"Library"
+        )
+    ]
+)
